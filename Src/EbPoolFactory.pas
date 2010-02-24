9303fb64533eb037187ca46cbb204e87ee549081
magnomp <magnomp@8e7203be-a26a-11de-bc7b-8ff00adae793>
2010-02-24 22:21:48 +0000

    Made some corrections on the LGPL header on all source files

--- Src/EbPoolFactory.pas
+++ Src/EbPoolFactory.pas
@@ -9,11 +9,11 @@
 
     Emballo is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>. }
+    along with Emballo. If not, see <http://www.gnu.org/licenses/>. }
 
 unit EbPoolFactory;
 
